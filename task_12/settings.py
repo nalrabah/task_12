09ca42235b8e6a7443021313ea22f8fe5c622d2e
nalrabah <nalrabah@hotmail.com>
2019-02-05 20:25:35 +0300

    ip

--- task_12/settings.py
+++ task_12/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tnvhv(22@t1#1!f9$&6gl#0-&9(symuxb$=x_2h6zkg*tdi4iy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [46.101.176.8]
+ALLOWED_HOSTS = ["46.101.176.8"]
 
 
 # Application definition
